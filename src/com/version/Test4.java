e7bac9a14dcda4d1648befd6f8b669ab36dd46b3
NLPSCOTT <wygchina@sina.com>
2014-10-28 16:02:52 +0800

    Test4.java add

---
+++ src/com/version/Test4.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.version;
+
+/**
+ * @author User
+ *
+ */
+public class Test4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
