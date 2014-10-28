7156fa30d00d6ff7a682aca72a531ec5675b93e1
NLPSCOTT <wygchina@sina.com>
2014-10-28 15:11:51 +0800

    version_3

--- src/com/version/Test.java
+++ src/com/version/Test.java
@@ -15,6 +15,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
            String name ="helo";
+           String weather = "sunshine";
+           boolean source = true;
 	}
 
 }
