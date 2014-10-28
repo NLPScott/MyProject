3fe7711e8afade8a6167834946f95a43ceb2aa55
NLPSCOTT <wygchina@sina.com>
2014-10-28 15:03:43 +0800

    添加天气

--- src/com/version/Test.java
+++ src/com/version/Test.java
@@ -15,6 +15,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
            String name ="helo";
+           String weather = "sunshine";
 	}
 
 }
