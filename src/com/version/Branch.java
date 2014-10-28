ef71eacf5fff1b8ed5a84b97b398e09c57eaf961
NLPSCOTT <wygchina@sina.com>
2014-10-28 16:42:56 +0800

    Branch Test6 add

---
+++ src/com/version/Branch.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.version;
+
+/**
+ * @author User
+ *
+ */
+public class Branch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
